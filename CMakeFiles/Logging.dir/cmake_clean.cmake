6ce4893a8e9d5def679ed3c4c20348ceb08bc59a
Konstantin <grib.konstanti-it21@it.dhbw-ravensburg.de>
2022-05-12 20:06:06 +0200

    created

---
+++ CMakeFiles/Logging.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Logging.dir/main.cpp.o"
+  "CMakeFiles/Logging.dir/main.cpp.o.d"
+  "Logging"
+  "Logging.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Logging.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
