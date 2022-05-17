2d7ae7d02a6d3be6063bcfe4a064faa49f22dbd2
Konstantin <grib.konstanti-it21@it.dhbw-ravensburg.de>
2022-05-17 11:27:38 +0200

    edit

--- src/logging.cpp
+++ src/logging.cpp
@@ -7,20 +7,20 @@
 
 #define BOOST_LOG_DYN_LINK
 #define BOOST_ALL_DYN_LINK
-#include <boost/log/core.hpp>
+//#include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>        //Fuer das Logging selber
-#include <boost/log/expressions.hpp>    //Fuer die Filter
+//#include <boost/log/expressions.hpp>    //Fuer die Filter
 //Fuer Ablage in Datei
 
 #include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
+//#include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
+//#include <boost/log/sources/severity_logger.hpp>
+//#include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/utility/setup/from_stream.hpp>
 
 //Fuer Zeitangabe
-#include <boost/log/support/date_time.hpp>
+//#include <boost/log/support/date_time.hpp>
 
 #include <fstream>
 
@@ -33,26 +33,12 @@ namespace keywords = boost::log::keywords;
 
 using namespace std;
 
-class timestamp_formatter_factory :
-        public boost::log::basic_formatter_factory<char, boost::posix_time::ptime>
-{
-    public:
-        formatter_type create_formatter(boost::log::attribute_name const& name, args_map const& args)
-        {
-            args_map::const_iterator it = args.find("format");
-            if (it != args.end())
-                return boost::log::expressions::stream << boost::log::expressions::format_date_time<boost::posix_time::ptime>(boost::log::expressions::attr<boost::posix_time::ptime>(name), it->second);
-            else
-                return boost::log::expressions::stream << boost::log::expressions::attr<boost::posix_time::ptime>(name);
-        }
-};
-
 int main(int, char*[])
 {
     cout << "Ausgabe erfolgt in einem Logging-File im selben Ordner" << endl;
 
     //Logging initialisation
-    boost::log::register_formatter_factory("TimeStamp", boost::make_shared<timestamp_formatter_factory>());
+    //boost::log::register_formatter_factory("TimeStamp", boost::make_shared<timestamp_formatter_factory>());
     boost::log::add_common_attributes();
 
     //Read logging settings from file
